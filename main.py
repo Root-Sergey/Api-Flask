f05f48950ecf940546dc7b08afafa5b7b55fcc76
Root-Sergey <briskostyle@gmail.com>
2023-04-29 22:07:14 +0300

    add req

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/python_req_1', methods=['GET'])
+def python_req_1():
+    name = request.args.get('name')
+    age = request.args.get('age')
+
+    salary = request.form.get('salary')
+    car = request.form.get('car')
+
+    result = {
+        'user_name': name,
+        'user_age': age,
+        'user_salary':salary,
+        'user_car': car
+    }
+
+    return jsonify(result)
+
