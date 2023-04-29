7f76dea0a69446830400fa1084c38fe00e9d8709
Root-Sergey <briskostyle@gmail.com>
2023-04-29 22:55:43 +0300

    add request

--- main.py
+++ main.py
@@ -19,5 +19,25 @@ def python_req_1():
         'user_car': car
     }
 
+    return jsonify(result), 404
+
+
+@app.route('/python_req_2', methods=['GET', 'POST'])
+def python_req_2():
+    model = request.args.get('model')
+    title = request.args.get('title')
+
+    price = int(request.form.get('price')) + 5000
+    open_air = request.form.get('open_air')
+
+    result = {
+        'model': model,
+        'title': title,
+        'price': price,
+        'open_air': open_air
+    }
+
     return jsonify(result)
+
+
 
